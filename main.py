423d5b77f5d9533861feaddb4cbc4677a5406f90
limstonestone <ehus1118@gmail.com>
2023-04-14 10:13:52 +0000

    k-fold 로 인한 FS 오류 수정

--- main.py
+++ main.py
@@ -76,6 +76,10 @@ def main(args):
         data['X_train'] = data['X_train'][features]
         data['X_valid'] = data['X_valid'][features]
         data['test'] = data['test'][features]
+        tr = pd.concat([data['X_train'], pd.DataFrame({'rating':data['y_train']})], axis=1)
+        val = pd.concat([data['X_valid'], pd.DataFrame({'rating':data['y_valid']})], axis=1)
+        data['train'] = pd.concat([tr, val])
+        del tr, val
 
     ######################## TRAIN
     print(f'--------------- {args.model} TRAINING ---------------')

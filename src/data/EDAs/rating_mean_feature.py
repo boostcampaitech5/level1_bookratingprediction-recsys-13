a683999e41c13b3b5fe256fd962fc4184ddfee8a
Hyunseok Cho <hoyajigi@gmail.com>
2023-04-17 15:04:30 +0000

    Change feature name

--- src/data/EDAs/rating_mean_feature.py
+++ src/data/EDAs/rating_mean_feature.py
@@ -24,17 +24,17 @@ def rating_mean_feature(users : pd.DataFrame, ratings1 : pd.DataFrame, ratings2 
     train_df_medians = ratings1.groupby('user_id')['rating'].median()
     train_df_total_median = ratings1['rating'].median()
 
-    users['rating_avg'] = users['user_id'].apply(lambda x: train_df_means[x] if x in train_df_means else train_df_total_mean)
-    users['rating_var'] = users['user_id'].apply(lambda x: train_df_vars[x] if x in train_df_vars else train_df_total_var).fillna(0)
-    users['rating_median'] = users['user_id'].apply(lambda x: train_df_medians[x] if x in train_df_medians else train_df_total_median)
+    users['user_rating_avg'] = users['user_id'].apply(lambda x: train_df_means[x] if x in train_df_means else train_df_total_mean)
+    users['user_rating_var'] = users['user_id'].apply(lambda x: train_df_vars[x] if x in train_df_vars else train_df_total_var).fillna(0)
+    users['user_rating_median'] = users['user_id'].apply(lambda x: train_df_medians[x] if x in train_df_medians else train_df_total_median)
 
-    ratings1['rating_avg'] = ratings1['user_id'].apply(lambda x: train_df_means[x] if x in train_df_means else train_df_total_mean)
-    ratings1['rating_var'] = ratings1['user_id'].apply(lambda x: train_df_vars[x] if x in train_df_vars else train_df_total_var).fillna(0)
-    ratings1['rating_median'] = ratings1['user_id'].apply(lambda x: train_df_medians[x] if x in train_df_medians else train_df_total_median)
+    ratings1['user_rating_avg'] = ratings1['user_id'].apply(lambda x: train_df_means[x] if x in train_df_means else train_df_total_mean)
+    ratings1['user_rating_var'] = ratings1['user_id'].apply(lambda x: train_df_vars[x] if x in train_df_vars else train_df_total_var).fillna(0)
+    ratings1['user_rating_median'] = ratings1['user_id'].apply(lambda x: train_df_medians[x] if x in train_df_medians else train_df_total_median)
 
-    ratings2['rating_avg'] = ratings2['user_id'].apply(lambda x: train_df_means[x] if x in train_df_means else train_df_total_mean)
-    ratings2['rating_var'] = ratings2['user_id'].apply(lambda x: train_df_vars[x] if x in train_df_vars else train_df_total_var).fillna(0)
-    ratings2['rating_median'] = ratings2['user_id'].apply(lambda x: train_df_medians[x] if x in train_df_medians else train_df_total_median)
+    ratings2['user_rating_avg'] = ratings2['user_id'].apply(lambda x: train_df_means[x] if x in train_df_means else train_df_total_mean)
+    ratings2['user_rating_var'] = ratings2['user_id'].apply(lambda x: train_df_vars[x] if x in train_df_vars else train_df_total_var).fillna(0)
+    ratings2['user_rating_median'] = ratings2['user_id'].apply(lambda x: train_df_medians[x] if x in train_df_medians else train_df_total_median)
     
     if is_dl:
         return users, books
